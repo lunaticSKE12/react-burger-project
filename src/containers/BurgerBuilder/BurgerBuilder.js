94163d64a6281b6a711102ad37efa6e0d24d74d7
Napong Dungduangsasitorn <jamesnapongd@gmail.com>
2020-03-06 17:06:26 +0700

    Order button enable

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -20,14 +20,13 @@ class BurgerBuilder extends Component {
   // }
 
   state = {
-    purchasable: false,
     purchasing: false,
     loading: false,
     error: false
   }
 
   componentDidMount() {
-    // console.log(this.props)
+    console.log(this.props)
     // axios.get('https://react-my-burger-701f0.firebaseio.com/ingredients.json')
     //   .then(response => {
     //     this.setState({ ingredients: response.data })
@@ -36,7 +35,6 @@ class BurgerBuilder extends Component {
   }
 
   updatePurchaseState(ingredients) {
-
     const sum = Object.keys(ingredients)
       .map(igkey => {
         return ingredients[igkey]
@@ -44,7 +42,7 @@ class BurgerBuilder extends Component {
       .reduce((sum, el) => {
         return sum + el
       }, 0)
-    this.setState({ purchasable: sum > 0 })
+    return sum > 0
   }
 
   purchaseHandler = () => {
@@ -85,7 +83,7 @@ class BurgerBuilder extends Component {
             ingredientAdded={this.props.onIngredientAdded}
             ingredientRemoved={this.props.onIngredientRemoved}
             disabled={disabledInfo}
-            purchasable={this.state.purchasable}
+            purchasable={this.updatePurchaseState(this.props.ings)}
             ordered={this.purchaseHandler}
             price={this.props.price} />
         </Aux>
