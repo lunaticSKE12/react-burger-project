7c65acd8e16ae654040aa408f63e9bd5dd4df374
Napong Dungduangsasitorn <jamesnapongd@gmail.com>
2020-03-10 23:40:42 +0700

    Test components

--- src/components/Navigation/NavigationItems/NavigationItems.test.js
+++ src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -8,8 +8,23 @@ import NavigationItem from './NavigationItem/NavigationItem'
 configure({ adapter: new Adapter() })
 
 describe('<NavigationItems />', () => {
+  let wapper
+  beforeEach(() => {
+    wapper = shallow(<NavigationItems />)
+  })
   it('should render two <NavigationItem /> elements if not authenticated', () => {
-    const wapper = shallow(<NavigationItems />)
+
     expect(wapper.find(NavigationItem)).toHaveLength(2)
+  })
+
+  it('should render three <NavigationItem /> elements if authenticated', () => {
+    // wapper = shallow(<NavigationItems isAuthenticated />)
+    wapper.setProps({ isAuthenticated: true })
+    expect(wapper.find(NavigationItem)).toHaveLength(3)
+  })
+
+  it('should render three <NavigationItem /> elements if authenticated', () => {
+    wapper.setProps({ isAuthenticated: true })
+    expect(wapper.contains(<NavigationItem link='/logout'>Logout</NavigationItem>)).toEqual(true)
   })
 })
