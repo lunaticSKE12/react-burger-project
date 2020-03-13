5d78a8bf8dc7a57399e98735e219207dcdf1f5ff
Napong Dungduangsasitorn <jamesnapongd@gmail.com>
2020-03-14 00:25:17 +0700

    Saga function call, all, takeLater

--- src/store/sagas/auth.js
+++ src/store/sagas/auth.js
@@ -1,12 +1,12 @@
-import { put, delay } from 'redux-saga/effects'
+import { put, delay, call } from 'redux-saga/effects'
 import axios from 'axios'
 
 import * as actions from '../actions/index'
 
 export function* logoutSaga(action) {
-  yield localStorage.removeItem('token')
-  yield localStorage.removeItem('expirationDate')
-  yield localStorage.removeItem('userId')
+  yield call([localStorage, 'removeItem'], 'token')
+  yield call([localStorage, 'removeItem'], 'expirationDate')
+  yield call([localStorage, 'removeItem'], 'userId')
   yield put(actions.logoutSucceed())
 }
 
@@ -28,7 +28,6 @@ export function* authUserSaga(action) {
   }
   try {
     const response = yield axios.post(url, authData)
-
     const expirationDate = yield new Date(new Date().getTime() + response.data.expiresIn * 1000)
     yield localStorage.setItem('token', response.data.idToken)
     yield localStorage.setItem('expirationDate', expirationDate)
